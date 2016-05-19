99237f0a9dca93ad294664d3dcd541ef1d299c5f
Gerardo Renovales <gerardo.renovales@shockoe.com>
2016-05-19 14:10:58 -0400

    A lot of updates for codelakeland, more to come!!

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,55 +1,93 @@
-function doClick(e) {
-    alert($.label.text);
+var TiBeacons = require('org.beuckman.tibeacons');
+
+function enterRegion(e) {
+	addData(e);
+}
+function exitRegion(e) {
+	addData(e);
+}
+function updateRanges(e) {
+	addData(e);
+}
+function handleProximity(e) {
+	addData(e);
+}
+
+function addListeners() {
+
+	TiBeacons.addEventListener("enteredRegion", enterRegion);
+	TiBeacons.addEventListener("exitedRegion", exitRegion);
+
+	TiBeacons.addEventListener("beaconRanges", updateRanges);
+	TiBeacons.addEventListener("beaconProximity", handleProximity);
+	
+}
+
+function removeListeners() {
+	
+	TiBeacons.removeEventListener("enteredRegion", enterRegion);
+	TiBeacons.removeEventListener("exitedRegion", exitRegion);
+
+	TiBeacons.removeEventListener("beaconRanges", updateRanges);
+	TiBeacons.removeEventListener("beaconProximity", handleProximity);
+}
+
+function pauseApp() {
+	TiBeacons.stopMonitoringAllRegions();
+	TiBeacons.stopRangingForAllBeacons();
+
+	removeListeners();
+};
+
+function appResumed(e) {
+	addListeners();
+}
+
+
+
+
+
+function stopMonitor(e) {
+	TiBeacons.stopMonitoringAllRegions();
 }
 
 function startMonitor(e){
 	Titanium.Geolocation.purpose="In order for this application to work, you must authorize the use of localization service.";
 	Titanium.Geolocation.getCurrentPosition(function(e){
-		if(e.error){}
-		else{
-			var TiBeacons = require('org.beuckman.tibeacons');
-			//TiBeacons.enableAutoRanging();
+		if(e.error){
 			
+		}
+		else{
 			TiBeacons.startMonitoringForRegion({
 			    uuid : "C48C6716-193F-477B-B73A-C550CE582A22",
 			    identifier : "com.bkon.detect"
-			});
-			
-			
-			TiBeacons.addEventListener("enteredRegion", function(e){
-				alert(e);
-			});
-			
-			
-			TiBeacons.addEventListener("exitedRegion", function(e){
-				alert(e);
-			});
-			
-			TiBeacons.addEventListener("beaconRanges", function(event) {
-			   alert(event.beacons);
-			});
-			
-			TiBeacons.addEventListener("beaconProximity", function(e){
-			   alert("beacon "+e.major+"/"+e.minor+" is now "+e.proximity);
 			});
 		}
 	});
 
 }
 
-function enterRegion(e) {
-	alert(e);
-}
-function exitRegion(e) {
-	alert(e);
-}
-function updateRanges(e) {
-	Ti.API.info(e);
-}
-function handleProximity(e) {
-	Ti.API.info(e);
-	
-	console.log("proximity: " + e.proximity);
+var preprocessForListView = function(rawData) {
+	return {
+		template: 'default',//(rawData.type != 'exitedRegion' || rawData.type != 'enteredRegion') ? 'default' :rawData.type,
+		time : {text: 'Date.now()'},
+		event: {text: ''},
+		uuid: {text: ''}
+	};
+};
+
+function addData(e){
+	if(e){
+		alert(e);
+		//var dataToAdd = preprocessForListView(e);
+		//Ti.API.info(dataToAdd);
+		//$.listView.sections[0].appendItems(dataToAdd);
+	}
 }
 
+
+
+Ti.App.addEventListener("pause", pauseApp);
+Ti.App.addEventListener("resumed", appResumed);
+addListeners();
 $.index.open();
